d305528622511bcb5c3be16ec843bcbb3dde9a17
kizzzil <kizzzil5@gmail.com>
2025-12-11 00:51:05 +0300

    run many triages

--- Xoctopus.py
+++ Xoctopus.py
@@ -17,28 +17,10 @@ def update_modules(os_name):
     # Далее тут будет проверка на валидность модулей.
     return module_names 
 
-@click.version_option("0.0.1", prog_name="Xoctopus")
-@click.command()
-@click.option('-t', default=None, help='Path to triage (target)')
-@click.option('-p', default=None, help='Use specific plugin plugin_name')
-def main(t, p):
-    if not t:
-        if os.listdir(f"{BASE_DIR}/cache/") == []:
-            click.echo('Please specify -t parametr')
-            sys.exit()
-        else:
-            with open(f"{BASE_DIR}/cache/latest.conf", "r") as latest:
-                t = latest.readline()
-    else:
-        if os.path.exists(f'{t}/[root]/'):
-            os_name = 'lin'
-        elif os.path.exists(f'{t}/Target/'):
-            os_name = 'win'
-        else: print(f'Ошибка чтения триажа по пути {t}')
-        os.environ["TRIAGE_NAME"] = t.split('/')[-1]
-        with open(f"{BASE_DIR}/cache/latest.conf", "w") as latest:
-            latest.write(t)
-     
+'''
+run_analyze() is a function, for analyze one triage
+''' 
+def run_analyze(os_name, p, t):
     #Переменная среды для передачи subrocess пути к триажу
     os.environ["TRIAGE_PATH"] = t
     module_names = update_modules(os_name) 
@@ -50,6 +32,49 @@ def main(t, p):
     else:
         print(f'module with name {p} undefined')
 
+def lin_or_win(triage_path):
+    if os.path.exists(f'{triage_path}/[root]/'):
+        return 'lin'
+    elif os.path.exists(f'{triage_path}/Target/'):
+        return 'win'
+    return None
+
+@click.version_option("0.0.1", prog_name="Xoctopus")
+@click.command()
+@click.option('-t', default=None, help='Path to triage (target)')
+@click.option('-p', default=None, help='Use specific plugin plugin_name')
+@click.option('-c', default=None, is_flag=True, help='Use current directory (Xoctopus.py/../) for analyze triages')
+def main(t, p, c):
+
+    
+    if c:
+        potential_triages = [f'{BASE_DIR}/../{triage_dir}' \
+                                for triage_dir in os.listdir(f'{BASE_DIR}/../')]
+        triages = {}
+
+        #filter unsuitable items
+        for path in potential_triages:
+            os_name = lin_or_win(path) 
+            if os_name is not None:
+                triages[path] = os_name
+
+        for path in triages.keys():
+            run_analyze(triages[path], p, path)
+    
+    elif t:
+        os_name = lin_or_win(t)
+        os.environ["TRIAGE_NAME"] = t.split('/')[-1]
+        with open(f"{BASE_DIR}/cache/latest.conf", "w") as latest:
+            latest.write(t) 
+        run_analyze(os_name, p, t)
+    else:
+        if os.listdir(f"{BASE_DIR}/cache/") == []:
+            click.echo('Please specify -t or -c parametr')
+            sys.exit()
+        else:
+            with open(f"{BASE_DIR}/cache/latest.conf", "r") as latest:
+                t = latest.readline()
+                run_analyze(os_name, p, t)
+
 if __name__ == "__main__":
     main()
-
