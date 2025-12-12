0dd25494020719715688de26f913709966e4d333
kizzzil <kizzzil5@gmail.com>
2025-12-12 11:41:55 +0300

    bagfix in zm_tools

--- modules/win/zimmerman_extractor/zimmerman_extractor.py
+++ modules/win/zimmerman_extractor/zimmerman_extractor.py
@@ -10,9 +10,9 @@ zmpath = os.environ["ZIMMERMAN_TOOLS_PATH"]
 '''
 ['path_to_cd', 'exename', 'filename', 'save_dir_path']
 '''
-command_data = [['Target/C/', 'MFTECmd.exe', '\$Extend/\$J" -m "\$MFT', '../../'], #$J and $MFT
-                ['Target/C/Windows/AppCompat/Programs/', 'AmcacheParser.exe', 'Amcache.hve', '../../../../../'], #Amcahce
-                ['Target/C/', 'MFTECmd.exe', '\\$MFT', '../../'] #MFT if $J is not work
+command_data = [['Target/C/', 'MFTECmd.exe', '\\$Extend/\\$J" -m "\\$MFT', '../../', 'MFT.csv'], #$J and $MFT
+                ['Target/C/Windows/AppCompat/Programs/', 'AmcacheParser.exe', 'Amcache.hve', '../../../../../', 'Amcache_csv.csv'], #Amcahce
+                ['Target/C/', 'MFTECmd.exe', '\\$MFT', '../../', 'MFT.csv'] #MFT if $J is not work
                 ]
 
 def main():
@@ -20,7 +20,7 @@ def main():
     for data in command_data:
         # Работа с MFT
         os.chdir(f'{triage}/{data[0]}')
-        cmd = f'{zmpath}/{data[1]} -f "{data[2]} " --csv "{data[3]}" > /dev/null'
+        cmd = f'{zmpath}/{data[1]} -f "{data[2]} " --csv "{data[3]}" --csvf "{data[4]}" > /dev/null'
         # print(cmd)
         os.system(cmd)
 
