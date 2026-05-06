2836591575711230aeae67ec69df1f747c1a8f39
kizzzil <kizzzil5@gmail.com>
2026-05-07 01:51:21 +0300

    extracting data for many disks

--- modules/parsers/win/zimmerman_extractor/zimmerman_extractor.py
+++ modules/parsers/win/zimmerman_extractor/zimmerman_extractor.py
@@ -10,25 +10,35 @@ zmpath = os.environ["ZIMMERMAN_TOOLS_PATH"]
 '''
 ['path_to_cd', 'exename', 'filename', 'save_dir_path']
 '''
-command_data = [['C/', 'MFTECmd.exe', '\\$Extend/\\$J" -m "\\$MFT', '../', 'MFT.csv'], #$J and $MFT
-                ['C/Windows/AppCompat/Programs/', 'AmcacheParser.exe', 'Amcache.hve', '../../../../', 'Amcache_csv.csv'], #Amcahce
-                ['C/', 'MFTECmd.exe', '\\$MFT', '../', 'MFT.csv'] #MFT if $J is not work
+command_data = [['', 'MFTECmd.exe', '\\$Extend/\\$J" -m "\\$MFT', '../', ''], #$J and $MFT
+                ['Windows/AppCompat/Programs/', 'AmcacheParser.exe', 'Amcache.hve', '../../../../', 'Amcache_csv.csv'], #Amcahce
+                ['', 'MFTECmd.exe', '\\$MFT', '../', ''] #MFT if $J is not work
                 ]
 
-custom_command = [[f'C/Windows/System32/LogFiles/SUM', f'{zmpath}/SumECmd.exe -d "." --csv ../../../../../Sum.csv > /dev/null']]
+custom_command = [[f'Windows/System32/LogFiles/SUM', f'{zmpath}/SumECmd.exe -d "." --csv ../../../../../Sum.csv > /dev/null']]
+
+disks = ['C/', 'D/', 'E/', 'F/']
 def main():
+    # Необходимо для работы триажей с несколькими дисками
+    for disk in disks:
+        if not os.path.exists(f'{triage}/{disk}'):
+            continue
+        else:
+            for data in command_data:
+                # По умолчанию меняет директорию на текущий диск
+                if os.path.exists(f'{triage}/{disk}/{data[0]}'):
+                    os.chdir(f'{triage}/{disk}/{data[0]}')
+                else:
+                    continue
+                cmd = f'{zmpath}/{data[1]} -f "{data[2]} " --csv "{data[3]}" --csvf "{data[4]}" > /dev/null'
+                os.system(cmd)
 
-    for data in command_data:
-        # Работа с MFT
-        os.chdir(f'{triage}/{data[0]}')
-        cmd = f'{zmpath}/{data[1]} -f "{data[2]} " --csv "{data[3]}" --csvf "{data[4]}" > /dev/null'
-        print(cmd)
-        os.system(cmd)
-
-    for cmd in custom_command:
-        print(cmd)
-        os.chdir(f'{triage}/{cmd[0]}')
-        os.system(cmd[1])
+            for cmd in custom_command:
+                if os.path.exists(f'{triage}/{disk}/{cmd[0]}'):
+                    os.chdir(f'{triage}/{disk}/{cmd[0]}')
+                else:
+                    continue
+                os.system(cmd[1])
 
 if __name__ == '__main__':
     main()
